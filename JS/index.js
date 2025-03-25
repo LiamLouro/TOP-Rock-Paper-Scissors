3ea6ca8b516581c1b42478f4ca8b82a7448184e0
LiamLouro <liam.louro14@gmail.com>
2025-03-25 21:51:03 +0100

    Add getComputerChoice function
    Takes a random number and returns a string ("rock", "paper", "scissors")

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,16 @@
-console.log('hola');
+// create the function
+function getComputerChoice() {
+// generate a random number and store it
+const result = Math.random()
+// depending on the number, return string "rock", "paper" or "scissors"
+if(result <= 0.33) {
+  console.log("rock")
+} else if
+  (result > 0.33 && result < 0.66) {
+  console.log('paper') 
+} else {
+  console.log('scissors');
+}
+}
+
+getComputerChoice()
